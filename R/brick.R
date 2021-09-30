b4181813308a1d46ad35e039f0a40eca298c64dd
b1019175 <Linda.Menk@sbg.ac.at>
2021-09-30 15:59:09 +0200

    First commit

---
+++ R/brick.R
@@ -0,0 +1,47 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' SPI spatial calculator
+#'
+#' @param path The local path to where the nedCDF file is located
+#' @param filename The filename of the netCDF. Must include the ending ".nc"
+#' @param x The length of the raster brick in its x dimension. Can be read from the netCDF info.
+#' @param y The length of the raster brick in its y dimension. Can be read from the netCDF info.
+#' @param z The length of the brick in its time dimension (number of brick layers).
+#'
+#' @return Returns a raster brick
+#' @export
+#'
+#'
+#'
+ncdf_brick <- function(path, filename, x, y, z){
+  ncpath <- path
+  ncname <- filename
+  ncfname <- paste0(ncpath, ncname)
+
+  ncin <- ncdf4::nc_open(ncfname)
+  print(ncin)
+
+  lon <- ncdf4::ncvar_get(ncin,"lon")
+  lat <- ncdf4::ncvar_get(ncin,"lat")
+
+  # Reading the precipitation data for the vegetation months.
+  # We have 575 x/lon and 297 y/lat values. 1572 is the original number of timesteps (including the whole year).
+  prec_array <- ncdf4::ncvar_get(ncin, "pr", c(1,1,1,1), c(x,y,z,1))
+  ncdf4::nc_close(ncin)
+
+  # Creating an empty brick with 297 rows, 575 columns and 786 layers (6 vegetation months ? 131 years)
+  raster::brick(nrows=y, ncols=x, xmn=min(lon), xmx=max(lon), ymn=min(lat), ymx=max(lat), nl=z, crs=4326)
+}
